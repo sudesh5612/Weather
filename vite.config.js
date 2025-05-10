c744ddf32443614739d14ff4348badbc7eef75a7
sudesh5612 <infodoc419@gmail.com>
2025-05-10 13:56:24 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Weather/',  // ✅ Correct path for GitHub Pages subfolder
-})
